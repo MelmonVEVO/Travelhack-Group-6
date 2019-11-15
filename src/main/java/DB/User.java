a7ba5c571ec0f21000ab98778b11ac7ecec55271
ZubaerRahman <52981368+ZubaerRahman@users.noreply.github.com>
2019-11-15 21:38:26 +0000

    adding initial db files

---
+++ src/main/java/DB/User.java
@@ -0,0 +1,14 @@
+package travelhack;
+
+public class User {
+	String username;
+	String password;
+	int exp;
+	public User(String un, String pwd) {
+		this.username= un;
+		this.password= pwd;
+		this.exp= 0;
+	}
+}
+
+
